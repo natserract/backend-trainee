812ea99a00a2236b7da9f26bd70ced70aeff577a
Alfin S <benjaminstwo@gmail.com>
2024-04-02 12:43:02 +0800

    feat: add logger packages

--- apps/node-ddd-ca/src/shared/infra/http/server.ts
+++ apps/node-ddd-ca/src/shared/infra/http/server.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import Koa from "koa";
-import { injectable } from "tsyringe";
+import { injectable, registry } from "tsyringe";
 
 import { APP_PORT } from "~/configs/app_config";
 
@@ -9,12 +9,20 @@ import { initModels } from "~/shared/infra/db/models/models";
 
 import registerApplicationMiddlewares from "~/shared/infra/http/middleware";
 import registerApplicationRouters from "~/shared/infra/http/controller";
+import { Logger, LOGGER_SYMBOLS } from "~/shared/packages/logger/logger";
 
 dotenv.config();
 
+@registry([
+  {
+    token: LOGGER_SYMBOLS,
+    useClass: Logger,
+  },
+])
 @injectable()
-export class HttpServer {
+export class HttpServer extends Logger {
   constructor() {
+    super("Application");
     this.init();
   }
 
@@ -31,7 +39,7 @@ export class HttpServer {
     await registerApplicationRouters(app);
 
     app.listen(APP_PORT, () => {
-      console.log(`Server available at http://localhost:${APP_PORT}`);
+      this.log(`Server available at http://localhost:${APP_PORT}`);
     });
   }
 }
